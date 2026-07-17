812b87240b5d007edbce34ef6aafbbd01ba67fcb
inyrdim <managerdims@gmail.com>
2026-07-17 11:35:43 +0800

    add drizzle

--- monorepo/lib/auth.ts
+++ monorepo/lib/auth.ts
@@ -1,12 +1,16 @@
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { betterAuth } from "better-auth";
-import { createPool } from "mysql2/promise";
+import { db } from "@/db";
 
 export const auth = betterAuth({
-    database: createPool({
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    }),
+  database: drizzleAdapter(db, {
+    provider: "mysql", // or "pg" or "mysql"
+  })
+    // database: createPool({
+    //     host: process.env.DB_HOST,
+    //     port: Number(process.env.DB_PORT),
+    //     user: process.env.DB_USER,
+    //     password: process.env.DB_PASSWORD,
+    //     database: process.env.DB_NAME,
+    // }),
 })
