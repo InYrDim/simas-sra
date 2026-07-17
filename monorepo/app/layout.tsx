7ba57aaaf0b81af0092d487972ec4f85eedee9d8
inyrdim <managerdims@gmail.com>
2026-07-17 14:22:37 +0800

    add tooltip provider to layout

--- monorepo/app/layout.tsx
+++ monorepo/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist, DM_Sans, Outfit } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-const outfitHeading = Outfit({subsets:['latin'],variable:'--font-heading'});
-
-const dmSans = DM_Sans({subsets:['latin'],variable:'--font-sans'});
+// example usage of creating custom font variables
+// const outfitHeading = Outfit({subsets:['latin'],variable:'--font-heading'});
+// const dmSans = DM_Sans({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "SIMAS - Sistem Informasi Manajemen Sekolah",
@@ -20,9 +20,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full antialiased", "font-sans", dmSans.variable, outfitHeading.variable)}
+      className={cn("h-full antialiased")}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+         <TooltipProvider >{children}</TooltipProvider>
+      </body>
     </html>
   );
 }
