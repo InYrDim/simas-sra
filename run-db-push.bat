73bb413cc3d94d30eda961b49cbc6cd58b304024
SERVER-JTIK <serverjtik@example.com>
2026-09-01 16:12:26 +0800

    cleanup

--- run-db-push.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd monorepo
-npx drizzle-kit push > "%USERPROFILE%\push-result.txt" 2>&1
-echo EXIT_CODE=%ERRORLEVEL% >> "%USERPROFILE%\push-result.txt"
--- run-db-push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd monorepo
-script -q /dev/null -c "pnpm db:push 2>&1" | sed 's/\x1b\[[0-9;]*m//g' > /tmp/db-push-result.txt
-echo "SCRIPT_DONE"
